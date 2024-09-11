5212bc51d4df1be7c3e0ee0d7f99b0597c2b8517
fruitbox12 <dylanwong007@gmail.com>
2024-09-11 13:58:25 -0700

    Update generateComponentNew.ts

--- server/utils/multipass/generateComponentNew.ts
+++ server/utils/multipass/generateComponentNew.ts
@@ -54,7 +54,7 @@ export default async (event: H3Event<EventHandlerRequest>) => {
   console.log(`> total context prompt tokens (estimate) : ${contextPromptToken}`)
 
   const stream = useOpenAI(event).beta.chat.completions.stream({
-    model: 'gpt-4-1106-preview',
+    model: 'gpt-4o',
     messages: context,
     stream: true,
   })
