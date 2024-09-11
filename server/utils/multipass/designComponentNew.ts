e11a740b5a743a608827b9526f05c50df3628ff1
fruitbox12 <dylanwong007@gmail.com>
2024-09-11 14:13:20 -0700

    Update designComponentNew.ts

--- server/utils/multipass/designComponentNew.ts
+++ server/utils/multipass/designComponentNew.ts
@@ -59,7 +59,7 @@ export default async (event: H3Event<EventHandlerRequest>) => {
   ]
 
   const stream = useOpenAI(event).beta.chat.completions.stream({
-    model: 'gpt-4o', // 'gpt-3.5-turbo-1106',
+    model: 'gpt-4-turbo', // 'gpt-3.5-turbo-1106',
     messages: context,
     tools: [
       {
